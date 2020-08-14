f5095e596c912d6053c600ec2a7ddee91112d0a7
Ram <ramos16qa@gmail.com>
2020-08-14 00:06:21 -0400

    home work 6 (commit 2)

--- src/com/dmilut/lesson_06/homework/HomeworkRam.java
+++ src/com/dmilut/lesson_06/homework/HomeworkRam.java
@@ -11,15 +11,14 @@ public class HomeworkRam {
             1.3. Инициализировать массив из п.1.1. с набором данных: 0,1,2,3,4,5,6,7,8,9 двумя различными путями
          */
 
-/*
         System.out.println("Задание 1");
 
 
-        int [] array;
-        String [] array2;
+        int[] array;
+        String[] array2;
 
         array = new int[10];
-        array2 = new String [10];
+        array2 = new String[10];
 
         array[0] = 0;
         array[1] = 1;
@@ -32,12 +31,10 @@ public class HomeworkRam {
         array[8] = 8;
         array[9] = 9;
 
-        int [] array3 = {0,1,2,3,4,5,6,7,8,9};
+        int[] array3 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
         System.out.println(array3[2]);
 
-
-*/
 
         System.out.println("Задание 2");
 
@@ -46,14 +43,12 @@ public class HomeworkRam {
             2.2. Вывести в консоль все элменты массива из п.2.1. так, чтобы получилась первоначальная строка
          */
 
-/*
 
-        String [] array4 = {"Я", "люблю", "массивы", "очень", "сильно","!"};
-        for(int i = 0; i < array4.length; i++) {
+        String[] array4 = {"Я", "люблю", "массивы", "очень", "сильно", "!"};
+        for (int i = 0; i < array4.length; i++) {
 
             System.out.print(array4[i] + " ");
         }
-*/
 
 
         System.out.println("Задание 3");
@@ -64,23 +59,20 @@ public class HomeworkRam {
             3.3. Сделать п.3.1. с циклом while
            */
 
-
-     /*   int[] array5 = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+        int[] array5 = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
         int z = 0;
-*/
-  /*      for (int a = 0; a < array5.length; a++) {
+        for (int a = 0; a < array5.length; a++) {
             System.out.println(array5[a]);
-        }*/
+        }
 
 
-       /* for (int b : array5) {
+        for (int b : array5) {
             System.out.println(array5[b]);
-        }*/
-     /*   while (z < array5.length){
+        }
+        while (z < array5.length) {
             System.out.println(array5[z]);
             z++;
         }
-*/
 
         System.out.println("Задание 4");
 
@@ -92,23 +84,26 @@ public class HomeworkRam {
          */
 
 
-      /*  int [] array6 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] array6 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
         for (int x = 0; x < array6.length; x++) {
             System.out.println(array6[x]);
-        }*/
+        }
 
 
-        /*int[] array7 = {10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
+        int[] array7 = {10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
         int q = 0;
 
         while (q < array7.length) {
             System.out.println(array7[q]);
             q++;
-*/
+
+
+        }
 
 
         System.out.println("Задание 5");
+
 
 
         // Middle level
@@ -118,41 +113,34 @@ public class HomeworkRam {
             Сделать это используя цикл.
          */
 
-
-      /*  String [] array8 = {"розовый", "черный", "красный", "желтый", "белый"};
-        int w = 0;
-        while (w < array8.length) {
-            System.out.println("Цвет настроения" + " " + array8[w]);
-            w++;
-        }*/
-
-
+        {
+            String[] array8 = {"розовый", "черный", "красный", "желтый", "белый"};
+            int w = 0;
+            while (w < array8.length) {
+                System.out.println("Цвет настроения" + " " + array8[w]);
+                w++;
+            }
 
 
-        System.out.println("Задание 6");
+            System.out.println("Задание 6");
 
          /* TODO: 8/12/20
             6.1. Найти и вывести в консоль наименьшее число из произвольного массива int
          */
 
 
-/*
+            int[] array10 = {15, 36, 98, 52, 14, 75, 42, 23, 10};
+            int n = 100;
 
-        int[] array10 = {15, 36, 98, 52, 14, 75, 42, 23, 10};
-        int n = 100;
-
-        for (int b : array10) {
-            if (b < n) {
-                n = b;
+            for (int b : array10) {
+                if (b < n) {
+                    n = b;
+                }
             }
-        }
-        System.out.println(n);
-
-*/
+            System.out.println(n);
 
 
-
-        System.out.println("Задание 7");
+            System.out.println("Задание 7");
 
 
         /* TODO: 8/12/20
@@ -160,56 +148,61 @@ public class HomeworkRam {
          */
 
 
- /*             int[] array9 = {15, 36, 98, 52, 14, 75, 42, 23, 10};
-        int m = 0;
-
-        for (int b : array9) {
-            if (b > m) {
-               m = b;
+            int[] array9 = {15, 36, 98, 52, 14, 75, 42, 23, 10};
+            int m = 0;
+            for (int b : array9) {
+                if (b > m) {
+                    m = b;
+                }
             }
-        }
-       System.out.println(m);*/
+            System.out.println(m);
 
 
-
-        System.out.println("Задание 8");
+            System.out.println("Задание 8");
 
 
         /* TODO: 8/12/20
             8.1. Найти и вывести в консоль только четные числа из произвольного массива int
          */
 
-    /*    int[] array11 = {15, 36, 98, 52, 14, 75, 42, 23, 10};
-        int m = 0;
+            int[] array11 = {15, 36, 98, 52, 14, 75, 42, 23, 10};
 
-        for (int b : array11) {
-            if (b % 2 == 0 ) {
-                System.out.println(b);
+
+            for (int b : array11) {
+                if (b % 2 == 0) {
+                    System.out.println(b);
+                }
             }
-        }*/
 
 
-        System.out.println("Задание 9");
+            System.out.println("Задание 9");
 
 
           /* TODO: 8/12/20
             9.1. Используя цикл for вывести все элементы массива [9, 8, 7, 6, 5, 4, 3, 2, 1, 0] в консоль в порядке
              возрастания
          */
-
+/*
         int[] array11 = {15, 36, 98, 52, 14, 75, 42, 23, 10};
-
-         for (int l = 0; l < array11.length; l++);
-
-                  System.out.println(array11[l]);
-
-
+        int m = 0;*/
+//         for (int l = 0; l < array11.length; l++){
+//
+//                  System.out.println(array11[l]);
 
 
-            }
-
+     /*   for (int b : array11) {
+            if (b > m) {
+                m = b;
+            } else if () {
+                System.out.println(array11[b]);
+*/
 
         }
     }
+}
+
+
+
+
 
 
