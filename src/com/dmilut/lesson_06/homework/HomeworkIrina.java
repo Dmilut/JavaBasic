b4c5364e842e799d5c717f2abd96cd99554e204c
irina0606 <irzhik1975@gmail.com>
2020-08-15 17:54:07 -0400

    saturday

--- src/com/dmilut/lesson_06/homework/HomeworkIrina.java
+++ src/com/dmilut/lesson_06/homework/HomeworkIrina.java
@@ -1,5 +1,7 @@
 package com.dmilut.lesson_06.homework;
 
+import java.util.ArrayList;
+
 public class HomeworkIrina {
     public static void main(String[] args) {
 
@@ -9,6 +11,7 @@ public class HomeworkIrina {
             1.2. Создать массив из п.1.1. с размером 10
             1.3. Инициализировать массив из п.1.1. с набором данных: 0,1,2,3,4,5,6,7,8,9 двумя различными путями
          */
+        System.out.println("Task 1");
 
         int[] line;
         line= new int[10];
@@ -31,6 +34,7 @@ public class HomeworkIrina {
             2.1. Строку "Я люблю массивы очень сильно!" сохранить в массиве разбив по словам и символам
             2.2. Вывести в консоль все элменты массива из п.2.1. так, чтобы получилась первоначальная строка
          */
+        System.out.println("Task 2");
 
         char[]a;
         a=new char[2];
@@ -45,22 +49,32 @@ public class HomeworkIrina {
         b[4]="Я ";
         System.out.println(a[0]+b[4]+b[0]+b[1]+b[2]+b[3]+a[1]+a[0]);
 
+        System.out.println('\n');
+
 
         /* TODO: 8/12/20
             3.1. Используя цикл for вывести все элементы массива [9, 8, 7, 6, 5, 4, 3, 2, 1, 0] в консоль
             3.2. Сделать п.3.1. с циклом foreach
             3.3. Сделать п.3.1. с циклом while
-         */
+        */
+
+        System.out.println("Task 3");
+
         int[]array={9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
         for(int i=0;i<=9;i++){
-            System.out.print(array[i]+",");
+            System.out.print(array[i]+" ");
         }
         System.out.println('\n');
-        int[]array1={9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
-        for(int i:array1){
-            System.out.print(array[i]+",");
-
+        for ( int x : array) {
+            System.out.print(x+" ");
         }
+        System.out.println('\n');
+        int h=0;
+        while(h<array.length) {
+            System.out.print(array[h] + " ");
+            h++;
+        }
+        System.out.println('\n');
 
 
 
@@ -70,6 +84,32 @@ public class HomeworkIrina {
             {10, 11, 12, 13, 14, 15, 16, 17, 18, 19}
             в консоль
          */
+        System.out.println("Task 4 - way 1");
+        int[]arr1={0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[]arr2={10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
+
+
+        for(int q=0;q<=9;q++){
+            System.out.print(arr1[q]+" ");
+        }
+        System.out.println('\n');
+
+            for (int w=0; w<=9;w++){
+                System.out.print(arr2[w]+" ");
+            }
+        System.out.println('\n');
+        System.out.println("Task 4 - way 2");
+            for(int k:arr1){
+                System.out.print(k + " ");
+            }
+        System.out.println('\n');
+            int w=0;
+            do{
+                System.out.print(arr2[w]+" ");
+                w++;
+            }
+
+            while( w<=9);
 
         // Middle level
         /* TODO: 8/12/20
@@ -77,10 +117,44 @@ public class HomeworkIrina {
             5.2. Вывести в консоль строчку из песни "Цвет настроения синий!" меняя цвет на цвета из массива.
             Сделать это используя цикл.
          */
+        System.out.println('\n');
+        System.out.println("Task 5");
+
+        String[]colors={"blue","red","white","green","purple"};
+        String song= "Color of the mood is ";
+        for(int x=0; x<5;x++){
+            System.out.println(song+colors[x]);
+        }
+        // or
+
+        System.out.println("OR");
+        int f=0;
+        while(f<=4){
+            System.out.println(song+colors[f]);
+            f++;
+        }
+        System.out.println("OR");
+        for(String n:colors){
+            System.out.println(song+n);
+        }
+
+
 
         /* TODO: 8/12/20
             6.1. Найти и вывести в консоль наименьшее число из произвольного массива int
          */
+        System.out.println('\n');
+        System.out.println("Task 6");
+
+      /*  int[]random={4,5,9,48,99,17,24,47,200,1000};
+        ArrayList list= new ArrayList();
+        list.add(random);
+        for(int d:random){
+            if (d < int min) min = d;
+                System.out.println("Min number is " + d);
+            }
+        }*/
+
 
         /* TODO: 8/12/20
             7.1. Найти и вывести в консоль наибольшее число из произвольного массива int
