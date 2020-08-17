1687155a8f090acc2c8e2519cf06202ce5b7c6e5
dmilut <d.lutcenkojob@gmail.com>
2020-08-16 21:12:03 -0700

    ContactService is fixed

--- src/com/dmilut/lesson_06/project/addressbook/service/ContactService.java
+++ src/com/dmilut/lesson_06/project/addressbook/service/ContactService.java
@@ -1,25 +1,34 @@
 package com.dmilut.lesson_06.project.addressbook.service;
 
 import com.dmilut.lesson_06.project.addressbook.entity.Contact;
+import com.dmilut.lesson_06.project.addressbook.util.Storage;
 
 public class ContactService {
+    StorageService storageService = new StorageService();
 
-    public void createContact(){
+    public void createContact(String firstName, String lastName, long phoneNumber) {
         Contact contact = new Contact();
+        contact.firstName = firstName;
+        contact.lastName = lastName;
+        contact.phoneNumber = phoneNumber;
 
+
+        storageService.saveContact(contact);
     }
 
     public Contact getContactByFirstName(String firstName) {
-        Contact contact = new Contact();
+        Contact contact = storageService.getByFirstName(firstName);
 
         return contact;
     }
 
     public void updateContact(Contact contact) {
 
+        storageService.updateContact(contact);
     }
 
     public void deleteContact(Contact contact) {
 
+        storageService.deleteContact(contact);
     }
 }
