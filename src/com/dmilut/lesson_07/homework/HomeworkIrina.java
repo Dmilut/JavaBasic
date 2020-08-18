a7a2fe5213bad678e356c1997973afdb25e40029
dmilut <d.lutcenkojob@gmail.com>
2020-08-17 17:13:43 -0700

    Lesson_07 structure is added

---
+++ src/com/dmilut/lesson_07/homework/HomeworkIrina.java
@@ -0,0 +1,9 @@
+package com.dmilut.lesson_07.homework;
+
+public class HomeworkIrina {
+
+    public static void main(String[] args) {
+
+    }
+}
+
---
+++ src/com/dmilut/lesson_07/homework/HomeworkOlga.java
@@ -0,0 +1,16 @@
+package com.dmilut.lesson_07.homework;
+
+public class HomeworkOlga {
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
+
+
+
+
+
+
