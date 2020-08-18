34167a809a157669cef3211fbcb83d5718c2a739
dmilut <d.lutcenkojob@gmail.com>
2020-08-18 11:49:55 -0700

    Lesson 7 is added

--- src/com/dmilut/lesson_07/lesson/Lesson.java
+++ src/com/dmilut/lesson_07/lesson/Lesson.java
@@ -4,5 +4,18 @@ public class Lesson {
 
     public static void main(String[] args) {
 
+        Human vasia = new Human();
+        vasia.sex = 'f';
+
+        Human petia = new Human('m', 40);
+
+        //System.out.println(vasia.sex);
+
+        boolean a = vasia.isPlaying("Blue");
+
+        System.out.println(petia.sex);
+        System.out.println(petia.age);
+
+
     }
 }
