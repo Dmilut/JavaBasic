c93c87c55322ee24b1e6fb260cf160b403d95d94
dmilut <d.lutcenkojob@gmail.com>
2020-08-20 12:15:20 -0700

    Lesson 8 is added

---
+++ src/com/dmilut/lesson_08/lesson/Circle.java
@@ -0,0 +1,13 @@
+package com.dmilut.lesson_08.lesson;
+
+public class Circle extends Shape{
+
+    public int size;
+
+    @Override
+    public void draw() {
+        System.out.println("Круг");
+    }
+
+
+}
