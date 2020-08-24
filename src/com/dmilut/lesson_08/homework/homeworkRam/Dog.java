a3ca02875bdd05bf568c90f17dc7bd9f75e5bfb5
dmilut <d.lutcenkojob@gmail.com>
2020-08-24 12:44:42 -0700

    Refactoring

--- src/com/dmilut/lesson_08/homework/homeworkRam/Dog.java
+++ src/com/dmilut/lesson_08/homework/homeworkRam/Dog.java
@@ -2,6 +2,11 @@ package com.dmilut.lesson_08.homework.homeworkRam;
 
 public class Dog extends Animal {
 
+    /* TODO: 8/20/20
+        3.1. Реализовать класс Dog наследованный от Animal
+        3.2. Переопределить метод move, так, чтобы он выводил в консоль "Бег"
+        3.3. Реализовать метод bark */
+
     {
         System.out.println("Task 3");
     }
