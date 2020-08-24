c0028483ee3e4df5c9644ec8ebdafd7681f0d4c2
irina0606 <irzhik1975@gmail.com>
2020-08-24 19:10:38 -0400

    new

--- src/com/dmilut/lesson_08/homework/homeworkIrina/Animal.java
+++ src/com/dmilut/lesson_08/homework/homeworkIrina/Animal.java
@@ -15,36 +15,10 @@ public class Animal {
     public String type;
     public String color;
 
-
-    public void move(String move) {
-        System.out.println("Some movement");
-    }
-
-    public void eat() {
-        System.out.println("It likes to eat");
-    }
-    public void sleep() {
-        System.out.println("It likes to sleep");
-    }
-
-    /* TODO: 8/20/20
-      4.1. Создать объект класса Animal, инициализировать его поля и вывести их значения в консоль
-      4.2. У созданного объекта класса Animal вызвать методы eat, sleep и move */
     public Animal(String name, int age, String type, String color) {
-        Animal tiger = new Animal("Max ", 3, " striped ", " orange ");
-        System.out.println(name+age+type+color);
-    }
-    public void tigerEat(){
-        System.out.println("It eats meat");
-    }
-    public void tigerSleep(){
-        System.out.println("It sleeps in the cave");
-    }
-    public void tigerMove(){
-        System.out.println("It moves fast");
-    }
-    }
 
+    }
+}
 
 
 
--- src/com/dmilut/lesson_08/homework/homeworkIrina/Dog.java
+++ src/com/dmilut/lesson_08/homework/homeworkIrina/Dog.java
@@ -11,31 +11,15 @@ public class Dog extends Animal {
         super(name, age, type, color);
     }
 
-    @Override
-    public void move(String move) {
-        System.out.println("Running");
-    }
+//    @Override
+//    public void move(String move) {
+//        System.out.println("Running");
+//}
 
     public void bark(String bark) {
         System.out.println("Barking");
     }
-    /* TODO: 8/20/20
-        6.1. Создать объект класса Dog, инициализировать унаследованные поля и вывести их значения в консоль
-        6.2. У созданного объекта класса Dog вызвать методы eat, sleep, move, bark
-        6.3. У созданного объекта класса Dog вызвать унаследованный метод move */
 
-    Dog dog = new Dog("Betty", 6, "yorkie", "silver");
-    public void dogEat(){
-        System.out.println("It eats bone");
-    }
-    public void dogSleep(){
-        System.out.println("It sleeps on the bed");
-    }
-    public void dogMove(){
-        System.out.println("It dances cute");
-    }
-    public void dogBark(){
-        System.out.println("It barks loudly");
-    }
-    // не понимаю как это сделать  6.3. У созданного объекта класса Dog вызвать унаследованный метод move */
+
+
 }
