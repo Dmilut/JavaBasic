2360ef19580ae68ebd6ad77c6a7480eff87d5c71
dmilut <d.lutcenkojob@gmail.com>
2020-08-24 13:27:20 -0700

    Refactoring

--- src/com/dmilut/lesson_08/homework/homeworkIrina/Animal.java
+++ src/com/dmilut/lesson_08/homework/homeworkIrina/Animal.java
@@ -9,12 +9,10 @@ public class Animal {
         1.3. Реализовать метод move, так, чтобы он выводил в консоль "Какое-то движение"
         1.4. Реализовать методы eat, sleep */
 
-
     public String name;
     public int age;
     public String type;
     public String color;
-
 
     public void move(String move) {
         System.out.println("Some movement");
@@ -23,6 +21,7 @@ public class Animal {
     public void eat() {
         System.out.println("It likes to eat");
     }
+
     public void sleep() {
         System.out.println("It likes to sleep");
     }
@@ -30,20 +29,24 @@ public class Animal {
     /* TODO: 8/20/20
       4.1. Создать объект класса Animal, инициализировать его поля и вывести их значения в консоль
       4.2. У созданного объекта класса Animal вызвать методы eat, sleep и move */
+
     public Animal(String name, int age, String type, String color) {
         Animal tiger = new Animal("Max ", 3, " striped ", " orange ");
-        System.out.println(name+age+type+color);
+        System.out.println(name + age + type + color);
     }
-    public void tigerEat(){
+
+    public void tigerEat() {
         System.out.println("It eats meat");
     }
-    public void tigerSleep(){
+
+    public void tigerSleep() {
         System.out.println("It sleeps in the cave");
     }
-    public void tigerMove(){
+
+    public void tigerMove() {
         System.out.println("It moves fast");
     }
-    }
+}
 
 
 
--- src/com/dmilut/lesson_08/homework/homeworkIrina/Monkey.java
+++ src/com/dmilut/lesson_08/homework/homeworkIrina/Monkey.java
@@ -4,13 +4,11 @@ package com.dmilut.lesson_08.homework.homeworkIrina;
         2.1. Реализовать класс Monkey наследованный от Animal
         2.2. Переопределить метод move, так, чтобы он выводил в консоль "Лазанье по деревьям" */
 
-
 public class Monkey extends Animal {
 
     public Monkey(String name, int age, String type, String color) {
         super(name, age, type, color);
     }
-
 
     @Override
     public void move(String move) {
@@ -22,17 +20,21 @@ public class Monkey extends Animal {
         5.1. Создать объект класса Monkey, инициализировать унаследованные поля и вывести их значения в консоль
         5.2. У созданного объекта класса Monkey вызвать методы eat, sleep и move */
 
-    Monkey monkey=new Monkey("Dasha", 10, "gorilla", "black");
-    public static void main(){
-       // System.out.println(name, age, type, color);
+    Monkey monkey = new Monkey("Dasha", 10, "gorilla", "black");
+
+    public static void main() {
+        // System.out.println(name, age, type, color);
     }
-    public void monkeyEat(){
+
+    public void monkeyEat() {
         System.out.println("It eats bananas");
     }
-    public void monkeySleep(){
+
+    public void monkeySleep() {
         System.out.println("It sleeps like person");
     }
-    public void monkeyMove(){
+
+    public void monkeyMove() {
         System.out.println("It jumps funny");
     }
 }
