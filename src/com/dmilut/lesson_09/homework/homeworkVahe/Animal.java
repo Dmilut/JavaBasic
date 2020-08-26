140734be2c852a49c0015eaf8d91c4ed34a3de1c
dmilut <d.lutcenkojob@gmail.com>
2020-08-26 12:48:10 -0700

    Refactoring

--- src/com/dmilut/lesson_09/homework/homeworkVahe/Animal.java
+++ src/com/dmilut/lesson_09/homework/homeworkVahe/Animal.java
@@ -4,7 +4,7 @@ package com.dmilut.lesson_09.homework.homeworkVahe;
         1.1. Объявить абстрактный класс Animal
         1.2. Объявить абстрактный метод voice */
 
-    public abstract class Animal {
+public abstract class Animal {
 
     public abstract void voice();
 
--- src/com/dmilut/lesson_09/homework/homeworkVahe/Cat.java
+++ src/com/dmilut/lesson_09/homework/homeworkVahe/Cat.java
@@ -1,53 +1,56 @@
 package com.dmilut.lesson_09.homework.homeworkVahe;
 
 /* TODO: 8/24/20
-        3.1. Объявить класс Cat наследованный от Animal
-        3.2. Объявить метод voice */
+    3.1. Объявить класс Cat наследованный от Animal
+    3.2. Объявить метод voice */
+public class Cat extends Animal {
 
-    public class Cat extends Animal {
+    private String name;
+    private int age;
+    private int weight;
+    private String type;
 
-        @Override
-        public void voice() {
-            System.out.println("Meow");
-        }
+    @Override
+    public void voice() {
+        System.out.println("Meow");
+    }
 
-
-/* TODO: 8/24/20
+    /* TODO: 8/24/20
         5.1. Добавить в класс Cat поля name, age, weight, type
         5.2. Реализовать геттеры и сеттеры для этих полей */
 
-        private String name;
-        public String getName() {
-            return name;
-        }
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        private int age;
-        public int getAge() {
-            return age;
-        }
-        public void setAge(int age) {
-            this.age = age;
-        }
-
-        private int weight;
-        public int getWeight() {
-            return weight;
-        }
-        public void setWeight(int weight) {
-            this.weight = weight;
-        }
-
-        private String type;
-        public String getType() {
-            return type;
-        }
-        public void setType(String type) {
-            this.type = type;
-        }
+    public String getName() {
+        return name;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
 
 
 
