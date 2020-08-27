7ba13ab4f866f887caf53450febc217a0232a78d
dmilut <d.lutcenkojob@gmail.com>
2020-08-26 21:33:43 -0700

    Lesson 10 is added

--- src/com/dmilut/lesson_10/lesson/Lesson.java
+++ src/com/dmilut/lesson_10/lesson/Lesson.java
@@ -2,34 +2,8 @@ package com.dmilut.lesson_10.lesson;
 
 public class Lesson {
 
-<<<<<<< HEAD
-    protected int test = 10;
-
     public static void main(String[] args) {
 
-        /* TODO: 8/26/20
-            Animal
-            Insect
-
-            Swimmable
-            Movable
-            Flyable
-
-            Mammal
-            Reptile
-            Fish
-            Bird */
-
-
-        Bug bug = new Bug();
-        /*bug.getColor();
-
-        System.out.println(bug.getColor());*/
-
-=======
-    public static void main(String[] args) {
-
->>>>>>> 9c9eb9f... Lesson 10 structure is added
     }
 
 }
