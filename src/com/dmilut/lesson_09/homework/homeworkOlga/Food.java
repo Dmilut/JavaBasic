2b755dc5a0543019007f18774230a929c7488cf0
dmilut <d.lutcenkojob@gmail.com>
2020-08-28 15:16:54 -0700

    Homeworks is updated

---
+++ src/com/dmilut/lesson_09/homework/homeworkOlga/Food.java
@@ -0,0 +1,23 @@
+package com.dmilut.lesson_09.homework.homeworkOlga;
+
+public class Food {
+
+    private String name;
+    private String type;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
