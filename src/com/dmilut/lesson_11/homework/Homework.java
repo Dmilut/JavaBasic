1e0c013e88e77bc9e87201a62c0fe337c8c78a6c
dmilut <d.lutcenkojob@gmail.com>
2020-08-31 21:10:06 -0700

    Lesson 11 is added

--- src/com/dmilut/lesson_11/homework/Homework.java
+++ src/com/dmilut/lesson_11/homework/Homework.java
@@ -2,4 +2,8 @@ package com.dmilut.lesson_11.homework;
 
 public class Homework {
 
+    /* TODO: 8/31/20
+        1.1. Реализовать метод обрабатывающий исключение при делении числа на 0
+     */
+
 }
