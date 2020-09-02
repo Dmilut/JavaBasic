e63e4068167810aecf21b63bd899a79411ad33ff
Vahe Gemilyan <vahe.gemilyan@gmail.com>
2020-09-01 23:07:33 -0700

    Homework 11 Vahe update

--- src/com/dmilut/lesson_11/homework/homeworkVahe/MyException.java
+++ src/com/dmilut/lesson_11/homework/homeworkVahe/MyException.java
@@ -1,7 +1,4 @@
 package com.dmilut.lesson_11.homework.homeworkVahe;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class MyException extends Exception {
 
