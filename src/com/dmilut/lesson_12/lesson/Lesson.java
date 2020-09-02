c10a08414dff30b85c8e0b4ab0479d720b512d6c
dmilut <d.lutcenkojob@gmail.com>
2020-09-02 15:44:45 -0700

    Lesson 12 clone demo is added

--- src/com/dmilut/lesson_12/lesson/Lesson.java
+++ src/com/dmilut/lesson_12/lesson/Lesson.java
@@ -1,8 +1,28 @@
 package com.dmilut.lesson_12.lesson;
 
+import com.dmilut.lesson_12.lesson.cloneDemo.Car;
+import com.dmilut.lesson_12.lesson.cloneDemo.Driver;
+
 public class Lesson {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CloneNotSupportedException {
 
+        Car car = new Car("Грузовик", new Driver("Василий", 45));
+        Car copiedCar = car;
+
+        Car clonedCar = car.clone();
+        System.out.println("Оригинал:\t" + car);
+        System.out.println("Копия:\t\t" + car);
+        System.out.println("Клон:\t\t" + clonedCar);
+
+        Driver clonedCarDriver = clonedCar.getDriver();
+        clonedCarDriver.setName("Вася");
+
+        System.out.println("Оригинал после изменения имени водителя:\t" + car);
+        System.out.println("Копия после изменения имени водителя:\t\t" + copiedCar);
+        System.out.println("Клон после изменения имени водителя:\t\t" + clonedCar);
+
+        System.out.println("Имя водителя:\t\t\t" + car.getDriver());
+        System.out.println("Имя водителя у клона:\t" + clonedCar.getDriver());
     }
 }
