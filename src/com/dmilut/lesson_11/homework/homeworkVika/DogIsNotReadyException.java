91cb0480a3f96bda8fa4b74cafcc38cda1b26e62
viktoriia1975 <prudka33@gmail.com>
2020-09-02 16:06:31 -0400

    Vika homework

---
+++ src/com/dmilut/lesson_11/homework/homeworkVika/DogIsNotReadyException.java
@@ -0,0 +1,8 @@
+package com.dmilut.lesson_11.homework.homeworkVika;
+
+public class DogIsNotReadyException extends Exception {
+
+    public DogIsNotReadyException(String message) {
+        super(message);
+    }
+}
