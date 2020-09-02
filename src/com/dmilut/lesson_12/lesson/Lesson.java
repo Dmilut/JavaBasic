50b77299ca9195d895cda4438b0c722b548b284c
dmilut <d.lutcenkojob@gmail.com>
2020-09-02 16:29:01 -0700

    Object's comparing example is added

--- src/com/dmilut/lesson_12/lesson/Lesson.java
+++ src/com/dmilut/lesson_12/lesson/Lesson.java
@@ -2,10 +2,32 @@ package com.dmilut.lesson_12.lesson;
 
 import com.dmilut.lesson_12.lesson.cloneDemo.Car;
 import com.dmilut.lesson_12.lesson.cloneDemo.Driver;
+import com.dmilut.lesson_12.lesson.compareDemo.Person;
 
 public class Lesson {
 
     public static void main(String[] args) throws CloneNotSupportedException {
+
+        cloneDemo();
+        compareDemo();
+
+    }
+
+    private static void compareDemo() {
+
+        Person person1 = new Person("Петров Иван Иванович", 56, false);
+        Person person2 = new Person("Петров Иван Иванович", 56, false);
+        Person person3 = person1;
+
+        System.out.println("person1 == person2?\t\t\t" + (person1 == person2));
+        System.out.println("person1 == person3?\t\t\t" + (person1 == person3));
+
+        System.out.println("person1.equals(person2)?\t" + person1.equals(person2));
+        System.out.println("person1.equals(person3)?\t" + person1.equals(person3));
+
+    }
+
+    private static void cloneDemo() throws CloneNotSupportedException {
 
         Car car = new Car("Грузовик", new Driver("Василий", 45));
         Car copiedCar = car;
@@ -24,5 +46,6 @@ public class Lesson {
 
         System.out.println("Имя водителя:\t\t\t" + car.getDriver());
         System.out.println("Имя водителя у клона:\t" + clonedCar.getDriver());
+
     }
 }
