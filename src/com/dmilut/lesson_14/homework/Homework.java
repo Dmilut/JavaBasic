42b5113b475a48dce51828b0112cc2b11c5cbfb5
dmilut <d.lutcenkojob@gmail.com>
2020-09-10 10:35:39 -0700

    Homework is added!

--- src/com/dmilut/lesson_14/homework/Homework.java
+++ src/com/dmilut/lesson_14/homework/Homework.java
@@ -1,4 +1,13 @@
 package com.dmilut.lesson_14.homework;
 
 public class Homework {
+
+    /* TODO: 9/10/20
+        1.1. Реализовать сортировку выбором с помощью рекурсии */
+
+    /* TODO: 9/10/20
+        2.1. Реализовать custom Array List (сделать свою реализацию) с методами add, remove, get, set */
+
+    /* TODO: 9/10/20
+        3.1. Реализовать custom Linked List (сделать свою реализацию) с методами add, remove, get, set */
 }
