2e85e1eca3669f2007c4fe0b98b4f0d06be006db
Ram <ramos16qa@gmail.com>
2020-09-11 00:51:23 -0400

    home work 14 (commit 1)

--- src/com/dmilut/lesson_13/lesson/Lesson.java
+++ src/com/dmilut/lesson_13/lesson/Lesson.java
@@ -4,38 +4,39 @@ public class Lesson {
 
     public static void main(String[] args) {
 
-        int[] shortTestArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+        int[] shortTestArray = {12, 2, 34, 4, 45, 6, 7, 8, 9, 10, 11};
         int[] longTestArray = longArrayBuilder(1_000_000);
 
-        long startTime = System.nanoTime();
-        System.out.println(binarySearch(shortTestArray, 9));
-        long finishTime = System.nanoTime();
-
-        double time1 = (finishTime - startTime) / 1_000_000D;
-        System.out.println("time = " + time1);
-
-        startTime = System.nanoTime();
-        System.out.println(binarySearch(longTestArray, 9));
-        finishTime = System.nanoTime();
-
-        double time2 = (finishTime - startTime) / 1_000_000D;
-        System.out.println("time = " + time2);
-
-
     }
-
-    // Iterative linear search, O(n)
-    public static int linearSearch(int[] array, int elementToSearch) {
-
-        // В цикле обходим все элементы массива и сравниваем с искомым
-        for (int index = 0; index < array.length; index++) {
-            if (array[index] == elementToSearch)
-                // Возвращаем индекс найденного элемента
-                return index;
-        }
-        // Если во время проверки ничего не нашли, то возвращаем -1
-        return -1;
-    }
+//        long startTime = System.nanoTime();
+//        System.out.println(binarySearch(shortTestArray, 9));
+//        long finishTime = System.nanoTime();
+//
+//        double time1 = (finishTime - startTime) / 1_000_000D;
+//        System.out.println("time = " + time1);
+//
+//        startTime = System.nanoTime();
+//        System.out.println(binarySearch(longTestArray, 9));
+//        finishTime = System.nanoTime();
+//
+//        double time2 = (finishTime - startTime) / 1_000_000D;
+//        System.out.println("time = " + time2);
+//
+//
+//    }
+//
+//    // Iterative linear search, O(n)
+//    public static int linearSearch(int[] array, int elementToSearch) {
+//
+//        // В цикле обходим все элементы массива и сравниваем с искомым
+//        for (int index = 0; index < array.length; index++) {
+//            if (array[index] == elementToSearch)
+//                // Возвращаем индекс найденного элемента
+//                return index;
+//        }
+//        // Если во время проверки ничего не нашли, то возвращаем -1
+//        return -1;
+//    }
 
 
     // Iterative binary search, O(lg(n))
@@ -81,5 +82,5 @@ public class Lesson {
         return array;
     }
 
-
 }
+
