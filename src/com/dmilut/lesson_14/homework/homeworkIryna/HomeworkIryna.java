4f280d13b4b7c61fb1a73a28d7dec2f64285d4f4
irina0606 <irzhik1975@gmail.com>
2020-09-13 21:07:12 -0400

    sunday

--- src/com/dmilut/lesson_14/homework/homeworkIryna/HomeworkIryna.java
+++ src/com/dmilut/lesson_14/homework/homeworkIryna/HomeworkIryna.java
@@ -1,8 +1,26 @@
 package com.dmilut.lesson_14.homework.homeworkIryna;
 
 public class HomeworkIryna {
-
     public static void main(String[] args) {
 
     }
+
+         /* TODO: 9/10/20
+        1.1. Реализовать сортировку выбором с помощью рекурсии */
+
+    public static int[] main() {
+        int[] sortArray = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+        for (int i = 0; i < sortArray.length; i++) {
+            System.out.println(sortArray[i] + " ");
+
+        }
+        return sortArray;
+    }
 }
+
+
+
+
+
+
+
---
+++ src/com/dmilut/lesson_14/homework/homeworkIryna/LinkedList.java
@@ -0,0 +1,6 @@
+package com.dmilut.lesson_14.homework.homeworkIryna;
+
+public class LinkedList {
+    /* TODO: 9/10/20
+        3.1. Реализовать custom Linked List (сделать свою реализацию) с методами add, remove, get, set */
+}
