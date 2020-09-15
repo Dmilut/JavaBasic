1e11a67f8955d801162a021c75eb0dbc8b7bb7e2
dmilut <d.lutcenkojob@gmail.com>
2020-09-14 21:09:27 -0700

    Homework is added

--- src/com/dmilut/lesson_15/homework/Homework.java
+++ src/com/dmilut/lesson_15/homework/Homework.java
@@ -2,6 +2,14 @@ package com.dmilut.lesson_15.homework;
 
 public class Homework {
 
+    /* TODO: 9/14/20
+        1.1. Реализовать custom stack и методы push, pop и peek
+     */
+
+    /* TODO: 9/14/20
+        2.1. Реализовать custom stack и методы push, pop и peek
+     */
+
     public static void main(String[] args) {
 
     }
