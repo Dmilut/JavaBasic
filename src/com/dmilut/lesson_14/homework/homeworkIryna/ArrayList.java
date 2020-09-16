78e1098c69e6de5e94657a99945470c9a8db55b3
irina0606 <irzhik1975@gmail.com>
2020-09-16 14:50:14 -0400

    wed

--- src/com/dmilut/lesson_14/homework/homeworkIryna/ArrayList.java
+++ src/com/dmilut/lesson_14/homework/homeworkIryna/ArrayList.java
@@ -1,38 +1,94 @@
 package com.dmilut.lesson_14.homework.homeworkIryna;
 
-public class ArrayList {
-    /* TODO: 9/10/20
+public class ArrayList <Fruit> {
+
+ /*TODO: 9/10/20
         2.1. Реализовать custom Array List (сделать свою реализацию) с методами add, remove, get, set */
-    public static void main(String[] args) {
-        String testString1="one";
-        String testString2="two";
-        String testString3="three";
 
 
-        ArrayList strings = new ArrayList();
-        strings.add(testString1,testString2,testString3);
-        System.out.println(testString1+" "+testString2+" "+testString3);
-        strings.remove(0);
-        System.out.println(testString2+" "+testString3);
-        strings.get(testString3);
-        System.out.println(testString3);
-        strings.set(2,testString3);
-        System.out.println();
+       /* private Fruit[] fruits;
+        private final int DEFAULT_ARRAY_LENGTH = 3;
 
 
+        public ArrayList() {
+          fruits = new <Fruit>[DEFAULT_ARRAY_LENGTH];
+        }
+
+
+        public ArrayList(int length) {
+          fruits = new <Fruit>[length];
+        }
+
+
+        public void add(Fruit element) {
+            int index = getFirstEmptySlotIndex();
+
+            if (index == fruits.length - 1) {
+                expandArray();
+            }
+
+            fruits[index] =(Fruit) element;
+        }
+
+
+        public void removeByIndex(int index) {
+
+            fruits[index] = null;
+        }
+
+
+        public void removeByElement(Fruit element) {
+            int index = 0;
+            for (int i = 0; i < fruits.length; i++) {
+                if (fruits[i].equals(element)) {
+                    index = i;
+                    break;
+                }
+            }
+
+            fruits[index] = null;
+        }
+
+
+        public Fruit get(int index) {
+
+            return fruits[index];
+        }
+
+
+        public void set(int index, Fruit element) {
+
+            fruits[index] = (Fruit)element;
+        }
+
+
+        public int length() {
+            return fruits.length;
+        }
+
+
+        private int getFirstEmptySlotIndex() {
+            int index = 0;
+
+            for (int i = 0; i < fruits.length; i++) {
+                if (fruits[i] == null) {
+                    index = i;
+                    break;
+                }
+            }
+            return index;
+        }
+
+
+        private void expandArray() {
+            Fruit[] tempArray = new <Fruit>[this.fruits.length + DEFAULT_ARRAY_LENGTH];
+
+            for (int i = 0; i < this.fruits.length; i++) {
+                tempArray[i] = this.fruits[i];
+            }
+
+            this.fruits = (Fruit[]) tempArray;
+        }
+
+*/
     }
-
-    private void set(int i, String testString3) {
-    }
-
-    private void get(String testString3) {
-    }
-
-    private void remove(int i) {
-    }
-
-    private void add(String testString1, String testString2, String testString3) {
-    }
-
-
-}
--- src/com/dmilut/lesson_14/homework/homeworkIryna/HomeworkIryna.java
+++ src/com/dmilut/lesson_14/homework/homeworkIryna/HomeworkIryna.java
@@ -3,19 +3,38 @@ package com.dmilut.lesson_14.homework.homeworkIryna;
 public class HomeworkIryna {
     public static void main(String[] args) {
 
+    int[]arr={10, 20,30,40,50,60,70,80,90,100};
+
     }
 
          /* TODO: 9/10/20
         1.1. Реализовать сортировку выбором с помощью рекурсии */
 
-    public static int[] main() {
-        int[] sortArray = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
-        for (int i = 0; i < sortArray.length; i++) {
-            System.out.println(sortArray[i] + " ");
-
+    public static void RecursionSelect(int[] arr, int length)
+    {
+        if(length==1)
+            return;
+        int max = 100;
+        int tmp;
+        int index = 0;
+        for(int i = 0;i<length-1;i++ )
+        {
+            if(arr[i]>max){
+                max = arr[i];
+                index = i;
+            }
         }
-        return sortArray;
+        if(max>arr[length-1])
+        {
+            tmp = arr[length-1];
+            arr[length - 1] = arr[index];
+            arr[index] = tmp;
+        }
+        RecursionSelect(arr, length-1);
     }
+
+
+
 }
 
 
