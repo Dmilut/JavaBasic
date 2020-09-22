198657960117b6eb5489827c44d5d86046d74183
irina0606 <irzhik1975@gmail.com>
2020-09-21 20:19:20 -0400

    mon

--- src/com/dmilut/lesson_16/homework/homeworkIryna/HomeworkIryna.java
+++ src/com/dmilut/lesson_16/homework/homeworkIryna/HomeworkIryna.java
@@ -2,26 +2,67 @@ package com.dmilut.lesson_16.homework.homeworkIryna;
 
 import java.util.Arrays;
 
+import static com.dmilut.lesson_16.lesson.Lesson.bubbleSort;
+
 public class HomeworkIryna {
 
     public static void main(String[] args) {
 
+        int[] testArray = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+
+        bubbleSortDes(testArray);
+        System.out.println(Arrays.toString(testArray));
+
         //getFibonacciSum(3,5);
         System.out.println(getFibonacciSum(4, 24));
-        /* TODO: 9/16/20
+    }
+
+
+    /* TODO: 9/16/20
         1.1. Реализовать сортировку пузырьком по убыванию */
+    public static void bubbleSortDes(int[] testArray) {
+        boolean isSorted = false;
+        int temp;
+
+        while (!isSorted) {
+            isSorted = true;
 
 
+            for (int i = 100; i > testArray.length; i--) {
+                for (int j = 1; j > testArray.length - 1; j--)
+                    if (testArray[j] > testArray[j + 1]) {
+                        isSorted = false;
 
-    /* TODO: 9/16/20
-        2.1. Реализовать сортировку пузырьком с помощью рекурсии */
+                        // Swap
+                        temp = testArray[j - 1];
+                        testArray[j + 1] = testArray[j];
+                        testArray[j] = temp;
+                    }
+            }
 
-    /* TODO: 9/16/20
-        3.1. Реализовать нахождение суммы чисел Фибоначчи заданного диапозона */
-
+        }
 
     }
 
+/* TODO: 9/16/20
+        2.1. Реализовать сортировку пузырьком с помощью рекурсии */
+
+    static void recBubbleSort(int my_arr2[], int len_arr) {
+        if (len_arr == 1)
+            return;
+        for (int i = 0; i < len_arr - 1; i++)
+            if (my_arr2[i] > my_arr2[i + 1]) {
+                int temp = my_arr2[i];
+                my_arr2[i] = my_arr2[i + 1];
+                my_arr2[i + 1] = temp;
+            }
+        recBubbleSort(my_arr2, len_arr - 1);
+
+    }
+
+
+    /* TODO: 9/16/20
+            3.1. Реализовать нахождение суммы чисел Фибоначчи заданного диапозона */
     public static int getFibonacciSum(int start, int finish) {
         if (finish >= 25) {
             return -1;
