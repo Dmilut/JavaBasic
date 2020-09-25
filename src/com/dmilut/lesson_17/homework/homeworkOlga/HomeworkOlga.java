b454935f00a0328f000e7c1f2feb913e7a59617e
dmilut <d.lutcenkojob@gmail.com>
2020-09-24 17:09:55 -0700

    Refactoring

--- src/com/dmilut/lesson_17/homework/homeworkOlga/HomeworkOlga.java
+++ src/com/dmilut/lesson_17/homework/homeworkOlga/HomeworkOlga.java
@@ -3,80 +3,83 @@ package com.dmilut.lesson_17.homework.homeworkOlga;
 import java.util.Random;
 
 public class HomeworkOlga {
-        /* TODO: 9/21/20
+    /* TODO: 9/21/20
         1.1. Провести отладку (дебагинг) метода quickSort из класса Lesson. Детально разобраться как он работает */
-//Done
+    //Done
 
-        /* TODO: 9/21/20
+    /* TODO: 9/21/20
         2.1. Используя подход "разделяй и властвуй" написать метод для поиска произвольного числа в массиве */
 
+    public static void main(String[] args) {
 
-    public static class QuickSort
-    {
-        public static int a = 20;
-        public static int[] sequence = new int[a];
+        int[] testArray = new int[20];
 
-        public static void quickSort(int left, int right)
-        {
-            if (right - left <= 0)
-                return;
-            else
-            {
-                Random rand = new Random();
-                int pivotIndex = left + rand.nextInt(right - left + 1);
-                swap(pivotIndex, right);
-
-                int pivot = sequence[right];
-                int partition = division(left, right, pivot);
-                quickSort(left, partition - 1);
-                quickSort(partition + 1, right);
-            }
+        for (int i = 0; i < testArray.length; i++) {
+            testArray[i] = Math.abs(new Random().nextInt(50));
         }
 
-        public static int division(int left, int right, long pivot)
-        {
-            int leftPtr = left - 1;
-            int rightPtr = right;
-            while (true)
-            {
-                while (sequence[++leftPtr] < pivot)
-                    ;
-                while (rightPtr > 0 && sequence[--rightPtr] > pivot)
-                    ;
+        printArray(testArray);
+        quickSort(testArray, 0, testArray.length - 1);
+        printArray(testArray);
+    }
 
-                if (leftPtr >= rightPtr)
-                    break;
-                else
-                    swap(leftPtr, rightPtr);
-            }
-            swap(leftPtr, right);
-            return leftPtr;
-        }
+    public static void quickSort(int[] array, int left, int right) {
+        if (right - left >= 0) {
+            int pivotIndex = left + new Random().nextInt(right - left + 1);
+            swap(array, pivotIndex, right);
 
-        public static void swap(int dex1, int dex2)
-        {
-            int temp = sequence[dex1];
-            sequence[dex1] = sequence[dex2];
-            sequence[dex2] = temp;
-        }
+            int pivot = array[right];
+            int partition = division(array, left, right, pivot);
 
-        static void printArray(int[] sorted_sequence)
-        {
-            for (int i = 0; i < sorted_sequence.length; i++)
-                System.out.print(sorted_sequence[i] + " ");
-        }
-        public static void main(String args[])
-        {
-            Random random = new Random();
-
-            for (int i = 0; i < a; i++)
-                sequence[i] = Math.abs(random.nextInt(50));
-            printArray(sequence);
-            quickSort(0, a- 1);
-            printArray(sequence);
+            quickSort(array, left, partition - 1);
+            quickSort(array, partition + 1, right);
         }
     }
+
+    private static int division(int[] array, int left, int right, long pivot) {
+        int leftPtr = left - 1;
+        int rightPtr = right;
+
+        while (true) {
+
+            do {
+                leftPtr++;
+            }
+            while (array[leftPtr] < pivot);
+
+            do {
+                rightPtr--;
+            }
+            while (rightPtr > 0 && array[rightPtr] > pivot);
+
+            if (leftPtr >= rightPtr) {
+                break;
+            } else {
+                swap(array, leftPtr, rightPtr);
+            }
+        }
+
+        swap(array, leftPtr, right);
+
+        return leftPtr;
+    }
+
+    private static void swap(int[] array, int index1, int index2) {
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+
+    private static void printArray(int[] array) {
+        for (int element : array) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
+    }
+
+
 }
+
 
 
 
